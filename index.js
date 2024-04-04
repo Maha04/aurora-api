370a62a5b4c84ead6866814c8aed637f8516b8f9
DESKTOP-DMH5THT\centi <mahalakshmi.k@centizen.com>
2024-04-04 15:43:36 +0530

    changes

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const app = express();
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-const app = express();
 
 app.use(express.json());
 app.use(cors());
