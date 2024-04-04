f5de9dc5038736865a8f381806a8df79bb46e036
DESKTOP-DMH5THT\centi <mahalakshmi.k@centizen.com>
2024-04-04 11:31:04 +0530

    removed console

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -3,16 +3,10 @@ import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.headers?.authorization;
-  console.log(token, "ACCESS TOKEN");
   if (!token) return next(errorHandler(401, "Unauthorized"));
-  console.log(token?.slice(7, token?.length), "AFTER SLICE");
-  jwt.verify(
-    token?.slice(7, token?.length),
-    process.env.JWT_SECRET,
-    (err, user) => {
-      if (err) return next(errorHandler(403, "Forbidden"));
-      req.user = user;
-      next();
-    }
-  );
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, "Forbidden"));
+    req.user = user;
+    next();
+  });
 };
